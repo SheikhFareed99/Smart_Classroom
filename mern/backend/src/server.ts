302b10e5010569fb83187f761c08ea7edffac082
MohammadIbraheem71 <ibraheemmuhamma71@users.noreply.github.com>
2026-03-05 22:18:00 +0500

    Revert "feature/login 1.1"
    This reverts commit da01d4a3a696358d27fdd94df885ab117ed9318a.

--- mern/backend/src/server.ts
+++ mern/backend/src/server.ts
@@ -2,11 +2,10 @@ import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import app from "./app";
 
 dotenv.config();
 
-
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
