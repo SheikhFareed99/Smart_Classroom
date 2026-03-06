851da4fd43a8a5fee95884a48f460195f4d24710
sheikh fareed <115920116+SheikhFareed99@users.noreply.github.com>
2026-03-06 20:43:00 +0500

    mongo db connected

--- mern/backend/src/server.ts
+++ mern/backend/src/server.ts
@@ -2,22 +2,16 @@ import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import app from "./app";
-
+import connectDB from './db'
 dotenv.config();
-
-
+connectDB()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// // MongoDB Connection
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mern_setup_db';
 
-// mongoose.connect(MONGODB_URI)
-//     .then(() => console.log('MongoDB connected'))
-//     .catch((err) => console.log(err));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from MERN Backend (TypeScript)!');
